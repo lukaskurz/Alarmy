b79f80ec23e1d0f3ab4bd18dcd9c37ba2f908884
lukaskurz <lukaskurz5@gmail.com>
2018-04-19 11:18:57 +0200

    added listening logic of data logger

--- logger/index.ts
+++ logger/index.ts
@@ -1,1 +1,9 @@
 "use strict"
+
+import * as mqtt from "mqtt";
+
+let client = mqtt.connect("mqtt://localhost:443");
+client.subscribe("#");
+client.on("message",(topic, message)=>{
+    console.log(`${topic.toString()}: ${message.toString()}`);
+})
