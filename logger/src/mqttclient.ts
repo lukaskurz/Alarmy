a6929ad3cea288ae0b84bfa6c570c288daf40172
lukaskurz <lukaskurz5@gmail.com>
2018-04-24 09:22:02 +0200

    fixed bug with inserting and add documentation

--- logger/src/mqttclient.ts
+++ logger/src/mqttclient.ts
@@ -7,16 +7,29 @@ export class MqttClient {
 	public Messages: Observable<MqttMessage>;
 	private Client: mqtt.MqttClient;
 
-	public constructor(config: MqttClientConfiguration, onMessage?: (message: string) => void) {
+	/**
+	 * Connects to the mqtt-broker and subscribes using the "#" wildcard to capture all messages being published.
+	 * Creates a cold observable for the messages beeing captured.
+	 * @param config Configuration containing Url and ports
+	 * @param onMessage Function that subscribes to the cold observable, making it a hot observable. If left empty, the observable stays cold
+	 */
+	public constructor(config: MqttClientConfiguration, onMessage?: (message: MqttMessage) => void) {
+		//Connects to the mqtt-broker
 		this.Client = mqtt.connect(`mqtt://${config.Url}:${config.AccessPort}`);
-		console.log("Connected to mqtt-broker")
+		//Captures all messages being published on any topic
 		this.Client.subscribe("#");
 
+		//Creates an cold observable for the messages being published
 		this.Messages = Rx.Observable.create((observer: Observer<MqttMessage>) => {
 			this.Client.on("message", (topic, message) => {
 				observer.next(new MqttMessage(-100,new Date(),message.toString(), topic));
 			});
 		});
+
+		//If the optional parameter is passed
+		if(onMessage){
+			this.Messages.subscribe(onMessage);
+		}
 	}
 
 }
