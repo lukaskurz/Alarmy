c027f159e029fb82b3b8f09dbfc787e9151d8402
Markus Leitenbauer <m.leitenbauer@students.htl-leonding.ac.at>
2018-04-24 12:33:15 +0200

    added sensor activation and testAlarm

---
+++ controller/classes/sensor.js
@@ -0,0 +1,11 @@
+
+class Sensor{
+    constructor(room, position, status, type){
+        this.room = room;
+        this.position = position;
+        this.status = status;
+        this.type = type;
+    }
+}
+
+module.exports = Sensor;
