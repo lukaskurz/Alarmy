441a9b0ea061c58b86ffe0a1f2945051fad24ce1
Markus Leitenbauer <m.leitenbauer@students.htl-leonding.ac.at>
2018-04-24 08:03:01 +0200

    Controller MQTT
    MQTT  - Upgrade
    Alarm alert to all actors after receiving a message from a sensor,

--- controller/app.js
+++ controller/app.js
@@ -4,28 +4,37 @@ const hostname = '127.0.0.1';
 const port = 1883;
 
 var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://vm61.htl-leonding.ac.at:1883');
+var client = mqtt.connect('localhost:1883');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
 });
 
 client.on('connect',function(){
     console.log('Connected');
-    client.subscribe('P2/Actors/#');
-    client.subscribe('P2/Sensors/#');
-    client.publish('P2/Sensors/E315','LightSensorAlert');
+    client.subscribe('P2/Alarmy/+/+/Sensor/#');
 })
 
 client.on('message',(topic, message) => {
-    if(topic === 'P2/Sensors/E315'){
-        console.log(message.toString());
+    if(topic === 'P2/Alarmy/+/+/Sensor/#'){
+        handleSensorMessage(topic,message)
     }
     client.end();
 })
 
+function handleSensorMessage(topic,message){
+    var MSGObj = JSON.parse(message);
+    alarmAlert();
+}
+
+function alarmAlert(){
+    client.publish('P2/Alarmy/+/+/Actor/#',JSON.stringify({
+        value: 'Alarm from Sensor',
+        time: Date.now().toString()
+    }));
+
+}
 
 
 server.listen(port, hostname, () => {
