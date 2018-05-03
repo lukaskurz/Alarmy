206d2b1d08859ac2a7230cd9a2915950694f2d1d
lukaskurz <lukaskurz5@gmail.com>
2018-05-03 15:26:31 +0200

    added some design

---
+++ web/alarmy-web/src/App/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FontIcon from 'material-ui/FontIcon';
+import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import ActionHistory from 'material-ui/svg-icons/action/history';
+import ActionHome from "material-ui/svg-icons/action/home";
+import ActionSettings from "material-ui/svg-icons/action/settings";
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import "./App.css";
+
+const GLOBALPROXY = {
+	URL: "localhost",
+	WEBSOCKETPORT: "8888",
+};
+
+class MessageJson {
+	constructor(messagetype, content) {
+		this.messagetype = messagetype;
+		this.content = content;
+		this.secret = localStorage.getItem("alarmy-secret");
+	}
+	messagetype = "";
+	content = "";
+	secret = "";
+}
+
+const iconStyles = {
+	marginRight: 24,
+};
+
+const bottomNav = {
+	position: "fixed",
+	bottom: 0,
+	width: "100vw"
+};
+
+
+export default class App extends Component {
+	connection = new WebSocket(`ws://${GLOBALPROXY.URL}:${GLOBALPROXY.WEBSOCKETPORT}`);
+	state = {
+		selectedIndex: 0,
+	};
+	constructor() {
+		super();
+		if (localStorage.getItem("alarmy-secret")) {
+			this.connection.onmessage = this.handleMessage;
+		} else {
+			this.noSecretFound();
+		}
+	}
+
+	select = (index) => this.setState({ selectedIndex: index });
+
+	noSecretFound() {
+
+	}
+
+	handleMessage(data) {
+		let message = JSON.parse(data);
+		switch (message.messagetype) {
+			case "stuff":
+			default:
+		}
+	}
+
+	render() {
+		return (
+			<MuiThemeProvider>
+				<Paper zDepth={1} style={bottomNav}>
+					<BottomNavigation className="bottomNav" selectedIndex={this.state.selectedIndex}>
+						<BottomNavigationItem
+							icon={<ActionHistory/>}
+							onClick={() => this.select(0)}
+						/>
+						<BottomNavigationItem
+							icon={<ActionHome/>}
+							onClick={() => this.select(1)}
+						/>
+						<BottomNavigationItem
+							icon={<ActionSettings/>}
+							onClick={() => this.select(2)}
+						/>
+					</BottomNavigation>
+				</Paper>
+			</MuiThemeProvider>);
+	}
+}
