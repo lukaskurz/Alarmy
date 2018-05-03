60c3303caf2c2485979b5e016e6ca0e01a63ef5d
lukaskurz <lukaskurz5@gmail.com>
2018-05-03 15:26:53 +0200

    forgot to save, so this should be part of last commit

--- web/alarmy-web/src/App/App.js
+++ web/alarmy-web/src/App/App.js
@@ -9,6 +9,13 @@ import ActionSettings from "material-ui/svg-icons/action/settings";
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import RaisedButton from 'material-ui/RaisedButton';
+import Card, { CardContent, CardMedia } from 'material-ui/Card';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+
 
 import "./App.css";
 
@@ -37,6 +44,33 @@ const bottomNav = {
 	bottom: 0,
 	width: "100vw"
 };
+
+const alarmCard = theme => ({
+	card: {
+	  display: 'flex',
+	},
+	details: {
+	  display: 'flex',
+	  flexDirection: 'column',
+	},
+	content: {
+	  flex: '1 0 auto',
+	},
+	cover: {
+	  width: 151,
+	  height: 151,
+	},
+	controls: {
+	  display: 'flex',
+	  alignItems: 'center',
+	  paddingLeft: theme.spacing.unit,
+	  paddingBottom: theme.spacing.unit,
+	},
+	playIcon: {
+	  height: 38,
+	  width: 38,
+	},
+  });
 
 
 export default class App extends Component {
