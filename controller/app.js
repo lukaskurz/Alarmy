49625024b777914ce77124fdf574009445327f1d
Markus Leitenbauer <m.leitenbauer@students.htl-leonding.ac.at>
2018-05-22 17:00:04 +0200

    added comments and websocket message listener

--- controller/app.js
+++ controller/app.js
@@ -1,3 +1,8 @@
+//---------------------------------
+//------- ALARMY CONTROLLER -------
+//Harwöck Florian, Kurz Lukas, Leeb Andreas, Leitenbauer Markus
+
+
 const http = require("http");
 
 const hostname = "127.0.0.1";
@@ -12,7 +17,7 @@ var client = mqtt.connect("mqtt://127.0.0.1:1883");
 
 var sensorList = [];
 
-//
+//Global proxy secret file + random generated secret if no one exists
 const secretFilename = "secret.txt";
 var secret = new randExp("[a-zA-Z0-9]{32}^").gen();
 if (fs.existsSync(secretFilename)) {
@@ -23,6 +28,8 @@ if (fs.existsSync(secretFilename)) {
 
 var websocketClient = new webSocketClient();
 
+
+//http connection for secret inspection
 const server = http
 	.createServer((req, res) => {
 		res.statusCode = 200;
@@ -40,10 +47,12 @@ server.on("request", (req, res) => {
 	}
 });
 
+//websocket error logging
 websocketClient.on("connectFailed", function(error) {
 	console.log("Websocket connection error: " + error.toString());
 });
 
+//Websocket message handling for the websocket connection
 websocketClient.on("connect", function(connection) {
 	console.log("Websocket client Connected");
 	connection.on("error", function(error) {
@@ -63,8 +72,12 @@ websocketClient.on("connect", function(connection) {
 	});
 });
 
+
+//connection to the global proxy
 websocketClient.connect("ws://globproxy.htl.harwoeck.at:8082/controller/" + secret);
 
+
+//mqtt subscription on sensor, client and controller topics
 client.on("connect", function() {
 	console.log("Mqtt connected");
 	client.subscribe("p2/alarmy/+/+/sensor/#");
@@ -72,8 +85,11 @@ client.on("connect", function() {
 	client.subscribe("p2/alarmy/controller/#");
 });
 
+
+//Decision whether there comes an mqtt message from a sensor or a client
 client.on("message", (topic, message) => {
 	console.log(`${topic}: ${message}`);
+
 	if (topic.toString().match("p2/alarmy/[A-z0-9]+/[A-z0-9]+/sensor/.+")) {
 		handleSensorMessage(topic, message);
 	} else if (topic.toString().match("p2/alarmy/controller/.+")) {
@@ -81,6 +97,8 @@ client.on("message", (topic, message) => {
 	}
 });
 
+
+//handling different client messages
 function handleClientMessage(topic, message) {
 	console.log(topic.toString().toLocaleLowerCase());
 	if (topic.toString().toLocaleLowerCase() === "p2/alarmy/controller/sensorstatus".toLocaleLowerCase()) {
@@ -88,6 +106,8 @@ function handleClientMessage(topic, message) {
 	}
 }
 
+//get the full ist of all sensors and prepare it as a json string with an 
+//array of JSON objcts packed in the content field
 function getSensorsAsJSON(){
 	var jsObj = JSON.parse(
 		'{ "type":300, "timestamp": "' + new Date().toDateString() + " " + new Date().toTimeString() + '", "content": []}'
@@ -100,6 +120,10 @@ function getSensorsAsJSON(){
 	return jsObj;
 }
 
+
+//handling the different types of sensor messages
+//activation -- sensor communicates with the controller for the firs time
+//alert -- sensor gets activated and sends an emergency alert to the controller
 function handleSensorMessage(topic, message) {
 	var MSGObj = JSON.parse(message);
 	var topArr = topic.split("/");
@@ -121,6 +145,8 @@ function handleSensorMessage(topic, message) {
 	}
 }
 
+
+//checks if a sensor is already inserted and activated in the sensor list
 function isSensorActivated(sensor) {
 	var found = false;
 	sensorList.forEach(function(element) {
