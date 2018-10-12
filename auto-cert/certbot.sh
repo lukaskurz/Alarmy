e37e72aefbc2d141ed73534f78783882e84eb6fa
lukaskurz <lukaskurz5@gmail.com>
2018-10-12 09:23:05 +0200

    changed folder name

--- auto-cert/certbot.sh
+++ auto-cert/certbot.sh
@@ -1,4 +1,4 @@
-sudo docker run -it --rm \
+sudo docker run -it \
 -v /docker-volumes/etc/letsencrypt:/etc/letsencrypt \
 -v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
 -v /docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt \
